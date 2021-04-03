6b6c75d21efe191554f6ec55d18ec623f9b8eff2
YubaC <2383931696@qq.com>
2021-04-03 09:50:01 +0800

    添加清明节问候语

--- js/dacss.js
+++ js/dacss.js
@@ -2,11 +2,12 @@ function getCSS() {
     datetoday = new Date();
     timenow = datetoday.getTime();
     datetoday.setTime(timenow);
-    myday= datetoday.getDate();
-    mymonth=datetoday.getMonth()+1;
-    if (myday == 4){
-        if (mymonth == 4){
-        display = "css/1.css";
+    myday = datetoday.getDate();
+    mymonth = datetoday.getMonth() + 1;
+    if (myday == 4) {
+        if (mymonth == 4) {
+            display = "css/1.css";
+            window.alert("草萋萋，雨绵绵，清明含泪祭奠；行缓缓，路漫漫，凝眸缭绕清烟；忆深深，思拳拳，难忘故人容颜；情切切，语千千，诉说此生挂牵。故人安息，生者康安。");
         }
     }
     var css = '<';
@@ -14,3 +15,4 @@ function getCSS() {
     css += '>';
     document.write(css);
 }
+
