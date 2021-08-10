ffe28307d61bec091c40e15a8847d28b75fb7b2a
YubaC <2383931696@qq.com>
2021-08-10 14:06:01 +0800

    添加用户访问统计

---
+++ js/tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?e41a2eac9bb9d4d70fb322f0838fd16b";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
