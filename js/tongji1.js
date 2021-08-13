d9b57a090c1ee6fa5646443427f5407466d316f3
YubaC <2383931696@qq.com>
2021-08-13 21:33:44 +0800

    更新了用户统计

---
+++ js/tongji1.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?bd14b93aa6b0c2e7194f1d700ba3f243";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
