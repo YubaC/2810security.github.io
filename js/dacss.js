f8d6bc2d545f21c89d38a7e1812e23471668df23
YubaC <2383931696@qq.com>
2022-02-09 14:40:49 +0800

    调整了清明节问候语和网页黑白化出现顺序

--- js/dacss.js
+++ js/dacss.js
@@ -7,12 +7,11 @@ function getCSS() {
     if (myday == 4) {
         if (mymonth == 4) {
             display = "css/1.css";
-            window.alert("草萋萋，雨绵绵，清明含泪祭奠；行缓缓，路漫漫，凝眸缭绕清烟；忆深深，思拳拳，难忘故人容颜；情切切，语千千，诉说此生挂牵。故人安息，生者康安。");
         }
     }
     var css = '<';
     css += 'link rel="stylesheet" href=' + display + ' \/';
     css += '>';
     document.write(css);
+    window.alert("草萋萋，雨绵绵，清明含泪祭奠；行缓缓，路漫漫，凝眸缭绕清烟；忆深深，思拳拳，难忘故人容颜；情切切，语千千，诉说此生挂牵。故人安息，生者康安。");
 }
-
