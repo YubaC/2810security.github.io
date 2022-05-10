27259ccc8d618e10d2d1eecda12f582839e9f6fc
YubaC <2383931696@qq.com>
2022-05-10 09:01:17 +0800

    进行CDN加速

--- images/校园风景/showname.py
+++ images/校园风景/showname.py
@@ -4,4 +4,4 @@ s = os.listdir(os.getcwd())
 
 with open('name.txt','a',encoding = 'utf-8') as f:
     for i in s:
-        f.write('<img width="100%" align="center" src="../images/校园风景/{0}"><br>\n'.format(i))
+        f.write('<img width="100%" align="center" src="https://cdn.jsdelivr.net/gh/YubaC/2810security.github.io@latest/images/校园风景/{0}"><br>\n'.format(i))
