0b88b5385dc6ed79deef491c29b6d885d3311820
YubaC <2383931696@qq.com>
2022-05-10 08:55:22 +0800

    添加了校园风景

---
+++ images/校园风景/showname.py
@@ -0,0 +1,7 @@
+import os
+
+s = os.listdir(os.getcwd())
+
+with open('name.txt','a',encoding = 'utf-8') as f:
+    for i in s:
+        f.write('<img width="100%" align="center" src="../images/校园风景/{0}"><br>\n'.format(i))
