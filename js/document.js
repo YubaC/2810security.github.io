4e1fe648310ec7ab9ecaf00c9b9f46236b4d1bab
YubaC <2383931696@qq.com>
2022-07-22 20:00:03 +0800

    添加了服务勋章

---
+++ js/document.js
@@ -0,0 +1,13 @@
+function IsPc() { //是PC→false，是移动端→true
+    let userAgent = navigator.userAgent,
+        Agents = ["Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod"];
+    console.log('userAgent:', userAgent);
+    return Agents.some((i) => {
+        return userAgent.includes(i);
+    })
+}
+
+if (IsPc()) {
+    document.getElementById("title_red").innerText = "济南舜耕中学二十八级十班安全局";
+    document.getElementById("titles").style.fontSize = "large";
+}
