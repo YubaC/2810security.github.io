ab89d51b4cfd5d08cb345a419fcc97430c200e60
YubaC <2383931696@qq.com>
2023-06-19 21:46:51 +0800

    Update header and footer

--- js/common.js
+++ js/common.js
@@ -1,6 +1,12 @@
 $(document).ready(() => {
-    // $("#header").load("header.html");
-    // $("#footer").load("footer.html");
-    $("#header").load("https://yubac.github.io/2810security.github.io/header.html");
-    $("#footer").load("https://yubac.github.io/2810security.github.io/footer.html");
+    $("#header").load("header.html", () => {
+        // 滚动条滚动到顶部，不要动画
+        $("html, body").animate({ scrollTop: 0 }, 0);
+    });
+    $("#footer").load("footer.html");
+    // $("#header").load("https://yubac.github.io/2810security.github.io/header.html", () => {
+    //     // 滚动条滚动到顶部，不要动画
+    //     $("html, body").animate({ scrollTop: 0 }, 0);
+    // });
+    // $("#footer").load("https://yubac.github.io/2810security.github.io/footer.html");
 });
