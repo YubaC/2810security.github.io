3a342bb0f5a522058a0cd07368e8b9e73e132071
YubaC <2383931696@qq.com>
2023-06-19 17:34:44 +0800

    Update index.htm and header and footer

--- js/common.js
+++ js/common.js
@@ -1,4 +1,6 @@
 $(document).ready(() => {
-    $("#header").load("header.html");
-    $("#footer").load("footer.html");
+    // $("#header").load("header.html");
+    // $("#footer").load("footer.html");
+    $("#header").load("https://yubac.github.io/2810security.github.io/header.html");
+    $("#footer").load("https://yubac.github.io/2810security.github.io/footer.html");
 });
